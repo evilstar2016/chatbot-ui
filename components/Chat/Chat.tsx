ff6fc4ab549d4775c0e400640ee4294ec3379c56
li_qzh <li-qzh@neusoft.com>
2023-05-13 12:47:25 +0800

    update

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -352,12 +352,12 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Chatbot UI
+            Welcome to Chatgpt
           </div>
           <div className="text-center text-lg text-black dark:text-white">
-            <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
+            <div className="mb-8">{`本项目克隆了chatgpt的UI界面`}</div>
             <div className="mb-2 font-bold">
-              Important: Chatbot UI is 100% unaffiliated with OpenAI.
+              重要：本项目与openAI无任何关系.
             </div>
           </div>
           <div className="text-center text-gray-500 dark:text-gray-400">
@@ -405,7 +405,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                         <Spinner size="16px" className="mx-auto" />
                       </div>
                     ) : (
-                      'Chatbot UI'
+                      'Chatgpt'
                     )}
                   </div>
 
